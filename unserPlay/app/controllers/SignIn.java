46bd6f7fac6b1e58c27e239d077cea8dfc70dc3d
Figo <figo1982@gmx.net>
2013-06-16 14:39:39 +0200

    Validierung mit JS eingebunden --> Makke und Figo Regex funktionieren nicht richtig --> Nadine Datenbankzugriff wird noch gemacht --> Makke und Figo
    Signed-off-by: Figo <figo1982@gmx.net>

--- unserPlay/app/controllers/SignIn.java
+++ unserPlay/app/controllers/SignIn.java
@@ -7,4 +7,19 @@ public class SignIn  extends Controller {
     public static Result index() {
         return ok(main.render(contentSignIn.render(), header.render(), navigation.render("signin"), footer.render(), sidebar.render("signin")));
     }
+    
+    public static Result signInValidation(){
+    	
+    	return ok(main.render(contentApp.render(), header.render(), navigation.render("app_map"), footer.render(), sidebar.render("app_map")));
+    }
+    
+    public static Result dbIn(){
+    	
+    	return ok();
+    }
+    
+    public static Result dbOut(){
+    	
+    	return dbOut();
+    }
 }
